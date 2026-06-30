c6c184d3643224603d1ebb449c646c4d61699c1f
Keshavcodes3 <Keshavcodes0@gmail.com>
2026-06-30 08:49:57 +0530

    feat:completing whole stuff

--- Backend/src/Services/graph.ai.service.ts
+++ Backend/src/Services/graph.ai.service.ts
@@ -6,6 +6,7 @@ import aiModels from "./models.service.js";
 import { providerStrategy, createAgent } from "langchain";
 
 const State = new StateSchema({
+  problem: z.string().default(""),
   messages: MessagesValue,
   solution_1: new ReducedValue(z.string().default(""), {
     reducer: (current, next) => next
@@ -17,9 +18,14 @@ const State = new StateSchema({
     z.object({
       solution_1_score: z.number(),
       solution_2_score: z.number(),
+      solution_1_reasonig: z.string().default(""),
+      solution_2_reasoning: z.string().default("")
+
     }).default({
       solution_1_score: 0,
       solution_2_score: 0,
+      solution_1_reasonig: "",
+      solution_2_reasoning: ""
     }),
     {
       reducer: (_, next) => next,
@@ -59,12 +65,12 @@ const judgeNode: GraphNode<typeof State> = async (state) => {
     const response = await judge.invoke({
       messages: [
         new HumanMessage(`
-Solution 1:
-${state.solution_1}
+          Solution 1:
+          ${state.solution_1}
 
-Solution 2:
-${state.solution_2}
-`)
+          Solution 2:
+          ${state.solution_2}
+          `)
       ]
     });
     console.log(response.structuredResponse)
